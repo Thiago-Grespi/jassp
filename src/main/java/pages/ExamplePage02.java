7bb88e8aafb39615297a2f931a8bbe96e5d5ff14
Thiago-Grespi <thiago.grespi90@gmail.com>
2019-03-24 01:14:01 -0300

    first commit

---
+++ src/main/java/pages/ExamplePage02.java
@@ -0,0 +1,11 @@
+package pages;
+
+import core.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static core.DriverFactory.getDriver;
+
+public class ExamplePage02 extends BasePage {
+
+}
